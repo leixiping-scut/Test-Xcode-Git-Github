dadffa07a9f05c6ffa3521c01b997ced808f7ef0
lxp <lxp@lxpdemacbook-air.local>
2016-01-29 15:20:05 +0800

    First try to commit to Github.

---
+++ TestOfGithub/TestOfGithub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestOfGithub
+//
+//  Created by lxp on 16/1/29.
+//  Copyright © 2016年 lxp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
